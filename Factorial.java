e97ba3171c8a1e445b6b65679b8431725d25baad
Mani Agarwal <42671984+Maniask@users.noreply.github.com>
2019-01-06 00:06:14 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -12,10 +12,10 @@ class Factorial
    {
 	 int fact=1;  
 	 Scanner sc=new Scanner(System.in);
-     System.out.println("Enter the number whose factorial you want to find");
-     int n=sc.nextInt();
-     for(int i=1;i<=n;i++)
-     fact=fact*i;	 
+         System.out.println("Enter the number whose factorial you want to find");
+         int n=sc.nextInt();
+         for(int i=1;i<=n;i++)
+         fact=fact*i;	 
 	 System.out.println("Factorial of the given number is: "+fact);
    }
 }   	 
